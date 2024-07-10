ab8195a090d7482f108ce3b7be3c82fcc7cc4540
HarrisonKanellos <harrisonkanellos@gmail.com>
2024-07-10 10:03:48 +1000

    Create playRound function to simulate 1 round
    If players choose the same, prints draw.
    If human wins, prints outcome and increments humanScore by 1.
    If computer wins, prints outcome and increments computerScore by 1.

--- script.js
+++ script.js
@@ -18,9 +18,60 @@ function getComputerChoice() {
 function getHumanChoice() {
     let hChoice;
     do {
-        hChoice = prompt("rock, paper, scissors");
+        hChoice = prompt("rock, paper, scissors").toLowerCase();
     }
     while (hChoice != "rock" && hChoice != "paper" && hChoice != "scissors")
     return hChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "paper") {
+                console.log("You lose! Paper wraps up the rock.");
+                computerScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log("You win! Rock crushes scissors.");
+                humanScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose rock.");
+            }
+            break;
+        case "paper":
+            if (computerChoice == "rock") {
+                console.log("You win! Paper wraps up the rock.");
+                humanScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log("You lose! Scissors cuts paper.");
+                computerScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose paper.");
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "rock") {
+                console.log("You lose! Rock crushes scissors.");
+                computerScore++;
+            }
+            else if (computerChoice == "paper") {
+                console.log("You win! Scissors cuts paper.");
+                humanScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose scissors.");
+            }
+            break;
+    }
+}
+
+let humanSelect = getHumanChoice();
+let computerSelect = getComputerChoice();
+
+console.log(humanSelect);
+console.log(computerSelect);
+
+playRound(humanSelect, computerSelect);
