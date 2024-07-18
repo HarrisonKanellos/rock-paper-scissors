a06a50741ce8599a72ce4678c939d5374d2cb784
HarrisonKanellos <harrisonkanellos@gmail.com>
2024-07-18 14:21:19 +1000

    Create buttons 3 selection buttons with event listeners

--- script.js
+++ script.js
@@ -20,73 +20,67 @@ function getHumanChoice() {
     return hChoice;
 }
 
-function playGame() {
-    // Score variables
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    // Function to play one round
-    function playRound(humanChoice, computerChoice) {
-        switch (humanChoice) {
-            case "rock":
-                if (computerChoice == "paper") {
-                    console.log("You lose! Paper wraps up the rock.");
-                    computerScore++;
-                }
-                else if (computerChoice == "scissors") {
-                    console.log("You win! Rock crushes scissors.");
-                    humanScore++;
-                }
-                else {
-                    console.log("It's a draw! You both chose rock.");
-                }
-                break;
-            case "paper":
-                if (computerChoice == "rock") {
-                    console.log("You win! Paper wraps up the rock.");
-                    humanScore++;
-                }
-                else if (computerChoice == "scissors") {
-                    console.log("You lose! Scissors cuts paper.");
-                    computerScore++;
-                }
-                else {
-                    console.log("It's a draw! You both chose paper.");
-                }
-                break;
-            case "scissors":
-                if (computerChoice == "rock") {
-                    console.log("You lose! Rock crushes scissors.");
-                    computerScore++;
-                }
-                else if (computerChoice == "paper") {
-                    console.log("You win! Scissors cuts paper.");
-                    humanScore++;
-                }
-                else {
-                    console.log("It's a draw! You both chose scissors.");
-                }
-                break;
-        }
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "paper") {
+                console.log("You lose! Paper wraps up the rock.");
+                computerScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log("You win! Rock crushes scissors.");
+                humanScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose rock.");
+            }
+            break;
+        case "paper":
+            if (computerChoice == "rock") {
+                console.log("You win! Paper wraps up the rock.");
+                humanScore++;
+            }
+            else if (computerChoice == "scissors") {
+                console.log("You lose! Scissors cuts paper.");
+                computerScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose paper.");
+            }
+            break;
+        case "scissors":
+            if (computerChoice == "rock") {
+                console.log("You lose! Rock crushes scissors.");
+                computerScore++;
+            }
+            else if (computerChoice == "paper") {
+                console.log("You win! Scissors cuts paper.");
+                humanScore++;
+            }
+            else {
+                console.log("It's a draw! You both chose scissors.");
+            }
+            break;
     }
-
-    // Select variables used in for loop
-    let humanSelect;
-    let computerSelect;
-
-    // Declare winner
-    if (humanScore > computerScore) {
-        console.log("You win Rock, Paper, Scissors!")
-    }
-    else if (humanScore < computerScore) {
-        console.log("You lose the game :(")
-    }
-    else {
-        console.log("The game is a draw!")
-    }
-
-    // Final results
-    console.log("---Here are the final scores---")
-    console.log("Your Score: " + humanScore);
-    console.log("Computer Score: " + computerScore)
 }
+
+let rockBtn = document.querySelector("#rock-button");
+let paperBtn = document.querySelector("#paper-button");
+let scissorsBtn = document.querySelector("#scissors-button");
+
+let rockVal = document.querySelector("#rock-button").value;
+let paperVal = document.querySelector("#paper-button").value;
+let scissorsVal = document.querySelector("#scissors").value;
+
+rockBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), rockVal);
+})
+paperBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), paperVal);
+})
+scissorsBtn.addEventListener('click', () => {
+    playRound(getComputerChoice(), scissorsVal);
+})
